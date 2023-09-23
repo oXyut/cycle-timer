533ba46c00d5aad425a683edf1fa949cc8b78978
oXyut <yuto.suzuki.0403@gmail.com>
2023-09-23 13:32:04 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+function IntervalTimer() {
+  const shortSound = () =>{
+   new Audio("button70.mp3").play(); 
+  }
+
+  const longSound = () =>{
+    new Audio("button79.mp3").play(); 
+  }
+
+  const [intervals, setIntervals] = useState([30, 30]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [timeLeft, setTimeLeft] = useState(intervals[0]);
+  const [cycleCount, setCycleCount] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+
+  const toggleTimer = () => {
+    setIsRunning(!isRunning);
+  };
+
+  useEffect(() => {
+    let timerId;
+    if (isRunning) {
+      timerId = setInterval(() => {
+        setTimeLeft(timeLeft - 1);
+      }, 1000);
+    }
+
+    if (timeLeft === 0) {
+      if (currentIndex + 1 < intervals.length) {
+        shortSound();
+        setCurrentIndex(currentIndex + 1);
+        setTimeLeft(intervals[currentIndex + 1]);
+      } else {
+        longSound();
+        setCurrentIndex(0);
+        setTimeLeft(intervals[0]);
+        setCycleCount(cycleCount + 1);
+      }
+    }
+
+    return () => clearInterval(timerId);
+  }, [timeLeft, currentIndex, intervals, cycleCount, isRunning]);
+
+  const resetCycleCount = () => {
+    setCycleCount(0);
+    setTimeLeft(intervals[0]);
+    setCurrentIndex(0);
+  };
+
+  const updateIntervals = (index, value) => {
+    const newIntervals = [...intervals];
+    newIntervals[index] = value;
+    setIntervals(newIntervals);
+  };
+
+  const removeLastInterval = () => {
+    const newIntervals = intervals.slice(0, -1);
+    setIntervals(newIntervals);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Interval Timer</h1>
+      <div>
+        {intervals.map((interval, index) => (
+          <input 
+            key={index}
+            type="number"
+            value={interval}
+            onChange={(e) => updateIntervals(index, parseInt(e.target.value))}
+          />
+        ))}
+        <button onClick={() => setIntervals([...intervals, 30])}>Add Interval</button>
+        <button onClick={removeLastInterval}>Remove Interval</button>
+      </div>
+      <button onClick={toggleTimer}>{isRunning ? 'Stop' : 'Start'}</button>
+      <div>
+        Time Left: {timeLeft} <br />
+        Current Cycle: {cycleCount}
+      </div>
+      <button onClick={resetCycleCount}>Reset Cycle Count</button>
     </div>
   );
 }
 
-export default App;
+export default IntervalTimer;
